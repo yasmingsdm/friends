5ab5e11a9365d74a49aeabe83c044c0ea0d17e62
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-03-27 15:50:59 +0200

    api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 
+import React from "react";
+
+import useFetch from './useFetch';
 import './App.css';
 
 function App() {
+const url= 'http://localhost:3001/friendsData'
+const anotherApi = 'https://jsonplaceholder.typicode.com/comments'
+  const {data, loading, error} = useFetch(url)
+
+  
+
   return (
-    <div className="App">
-      <h1>Friends</h1>
+    <div>
+      {loading && <p>Loading...</p>} 
+      {error ? <p>{error}</p> : console.log(data)}
     </div>
   );
 }
