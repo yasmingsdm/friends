bebc8b8773d45a1ee2c24ccc8dd5859a9fcd0e01
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-03-29 10:33:33 +0200

    css ok

--- my-app/src/Components/Characters.js
+++ my-app/src/Components/Characters.js
@@ -2,13 +2,15 @@ import React from "react";
 import Character from "./Character";
 
 const Characters = ({characters})=>{
-    const oneCharacter = characters && characters.map(character => <Character key = {character.id} character={character}/>)
+    const oneCharacter = characters && characters.map(character => <Character key = {character.id} character={character} />)
     return (
-        <section>
-            <h1>Friends Main Characters</h1>
+        <>
+        <h1 className="characters__title">Friends Main Characters</h1>
+         <section className='characters__section'>
             {oneCharacter}
-            
         </section>
+        </>
+       
        
     )
 }
